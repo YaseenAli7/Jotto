59ba105d46bbc5e8f63b4b0b68e69fa7d132fcf4
Yaseen Ali <105475030+YaseenAli7@users.noreply.github.com>
2023-02-01 18:35:20 -0500

    Update Jotto.py

--- Jotto.py
+++ Jotto.py
@@ -1,35 +1,11 @@
 """
 Author: Yaseen Ali, ali166@purdue.edu
-Assignment: 12.1 - Pyword
 Date: 04/19/2022
 
 Description:
     This program replicates a precursor to the game Wordle, called "Jotto"
-
-Contributors:
-    None
-
-My contributor(s) helped me:
-    [ ] understand the assignment expectations without
-        telling me how they will approach it.
-    [ ] understand different ways to think about a solution
-        without helping me plan my solution.
-    [ ] think through the meaning of a specific error or
-        bug present in my code without looking at my code.
-    Note that if you helped somebody else with their code, you
-    have to list that person as a contributor.
-
-Academic Integrity Statement:
-    I have not used source code obtained from any unauthorized
-    source, either modified or unmodified; nor have I provided
-    another student access to my code.  The project I am
-    submitting is my own original work.
 """
-
-"""Import additional modules below this line (starting with unit 6)."""
 import random
-
-"""Write new functions below this line (starting with unit 4)."""
 
 def getInput():
 
